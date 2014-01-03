d08740b7c0ca786c29a3de48cb8f2d14f0c923b8
Julio Makdisse Saito <saitodisse@gmail.com>
2014-01-03 17:48:28 -0200

    merge audio and video

--- you-helper.sh
+++ you-helper.sh
@@ -7,24 +7,39 @@ File1New=video.mp4
 File2New=audio.m4a
 
 URL=$1
+Qual1=$2
+Qual2=$3
+
 if [ -z $URL ]; then
-  echo "Usage: ytdl.sh url"
+  echo "Usage: bash you-helper.sh url 137 141"
   exit
 fi
 
 #Find what quality of videos are available
-youtube-dl -F $URL
-echo
-echo -n "Quality for Video (default 137): "
-read Qual1
-echo -n "Quality for Audio (default 141): "
-read Qual2
+if [ -z $Qual1 -o -z $Qual2 ]; then
+  echo
+  echo -n "Listing available videos sources"
+  echo
+  youtube-dl -F $URL
+fi
 
-#Set values if user has just pressed Return without typing anything
+if [ -z $Qual1 ]; then
+  echo
+  echo -n "Quality for Video (default 137): "
+  read Qual1
+fi
+# Set values if user has just pressed Return without typing anything
 if [ -z $Qual1 ]; then
   Qual1="137"
 fi
+
 if [ -z $Qual2 ]; then
+  echo
+  echo -n "Quality for Audio (default 141): "
+  read Qual2
+fi
+# Set values if user has just pressed Return without typing anything
+if [ -z $Qual1 ]; then
   Qual2="141"
 fi
 
@@ -33,45 +48,52 @@ File1=$(youtube-dl --get-filename -f $Qual1 $URL)
 File2=$(youtube-dl --get-filename -f $Qual2 $URL)
 echo $File1
 echo $File2
-Out="out_"$File1
-echo $Out
 
 #Download Video file with First Quality Setting
+echo
+echo "Getting Video!"
+echo
 youtube-dl -f $Qual1 $URL
 if [[ ! -f $File1 ]]; then
   echo
   echo "Error video file not downloaded"
   exit
 fi
-mv "$File1" "$File1New"
 
 #Download Audio file with Second Quality Setting
+echo
+echo "Getting Audio!"
+echo
 youtube-dl -f $Qual2 $URL
 if [[ ! -f $File2 ]]; then
   echo
   echo "Error audio file not downloaded"
   exit
 fi
-mv "$File2" "$File2New"
 
-File1=$File1New
-File2=$File2New
-
-#Merge Audio and Video with ffmpeg
-#Delete -threads 0 if you have a Single Core CPU
 echo
 echo "Combining Audio and Video files with MP4Box"
-MP4Box -add "$File1" "$File2"
-if [[ -f $Out ]]; then
+echo "MP4Box -add" $File1 $File2
+echo
+MP4Box -add "$File2" "$File1"
+
+if [[ -f "$File2" ]]; then
+  #Remove audio file
   echo
-  echo "File" $Out "created"
-else
+  echo "Remove audio file"
   echo
-  echo "Error Unable to combine Audio and Video files with FFMpeg"
-  exit
+  rm "$File2"
 fi
 
-#Remove old Files
-rm "$File1"
-rm "$File2"
+
+if [[ -f out_"$File1" ]]; then
+  #Back to original name
+  echo
+  echo "Back to original name"
+  echo "old name: " out_"$File1"
+  echo "new name: " "$File1"
+  echo
+  mv out_"$File1" "$File1"
+fi
+
 
